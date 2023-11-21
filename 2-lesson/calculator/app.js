92baedff8662a20438bff4f8cda571efca751853
Branden Ge <86636108+brandenge@users.noreply.github.com>
2023-11-21 14:48:15 -0600

    Feat: add user input validation and refactor

--- 2-lesson/calculator/app.js
+++ 2-lesson/calculator/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const readline = require('readline-sync');
+const rl = require('readline-sync');
 const Calculator = require('./calculator');
 
 class App {
@@ -12,27 +12,74 @@ class App {
     this.printResult(result);
   }
 
+  prompt(message) {
+    console.log(`=> ${message}`);
+  }
+
   printWelcome() {
-    console.log('Welcome to Calculator!');
+    this.prompt('Welcome to Calculator!');
   }
 
   getUserOperandsAndOperator() {
-    const num1 = parseFloat(readline.question("What's the first number? "));
-    const num2 = parseFloat(readline.question("What's the second number? "));
-    const operator = readline.question("What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide: ");
-
+    const num1 = this.getValidNumber(1);
+    const num2 = this.getValidNumber(2);
+    const operator = this.getValidOperator();
     const operations = {
       1: '+',
       2: '-',
       3: '*',
       4: '/',
     };
-
     return [num1, num2, operations[operator]];
   }
 
+  getValidNumber(orderNum) {
+    const orderWords = {
+      1: 'first',
+      2: 'second',
+    };
+    const order = orderWords[orderNum];
+    const numPrompt = `What is the ${order} number? `;
+    let num;
+
+    do {
+      this.prompt(numPrompt);
+      num = parseFloat(rl.question());
+      if (this.isInvalidNumber(num)) {
+        this.prompt("Hmmm...that doesn't look like a valid number.");
+      } else {
+        break;
+      }
+    } while (true);
+    return num;
+  }
+
+  getValidOperator() {
+    const operatorPrompt = 'What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide:';
+    let operator;
+
+    do {
+      this.prompt(operatorPrompt);
+      operator = rl.question();
+      if (this.isInvalidOperator(operator)) {
+        this.prompt('Must choose 1, 2, 3, or 4.');
+      } else {
+        break;
+      }
+    } while (true);
+    return operator;
+  }
+
+  isInvalidNumber(num) {
+    return Number.isNaN(num);
+  }
+
+  isInvalidOperator(operatorChoice) {
+    return !['1', '2', '3', '4'].includes(operatorChoice);
+  }
+
   printResult(result) {
-    console.log(`The result is: ${result}`);
+    this.prompt(`The result is: ${result}`);
   }
 }
 
