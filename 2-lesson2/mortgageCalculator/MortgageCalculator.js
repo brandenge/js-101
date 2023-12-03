e4d48124a81552894f8f5f942b7e62b2220f20d0
Branden Ge <86636108+brandenge@users.noreply.github.com>
2023-12-02 18:11:46 -0600

    Fix: bug for 0 interest rate returning NaN

--- 2-lesson2/mortgageCalculator/MortgageCalculator.js
+++ 2-lesson2/mortgageCalculator/MortgageCalculator.js
@@ -4,6 +4,7 @@ class MortgageCalculator {
   calcMonthlyPayment(loanAmount, annualPercentageRate, loanYears) {
     const loanMonths = loanYears * 12;
     const monthlyInterestRate = annualPercentageRate / 12;
+    if (annualPercentageRate === 0) return loanAmount / loanMonths;
     const monthlyPayment =
       (loanAmount * (monthlyInterestRate /
       (1 - ((monthlyInterestRate + 1) ** -loanMonths))));
